b14ca251b62d259804e6b72511fe93976c8862b2
Matt Wilkin <mwilkin22@gmail.com>
2019-06-10 16:58:24 -0700

    trying to fix login.

--- src/auth/middleware.js
+++ src/auth/middleware.js
@@ -25,7 +25,7 @@ module.exports = (req, res, next) => {
     let base64Buffer = Buffer.from(authString,'base64'); // <Buffer 01 02...>
     let bufferString = base64Buffer.toString(); // john:mysecret
     let [username,password] = bufferString.split(':');  // variables username="john" and password="mysecret"
-    let auth = [username,password];  // {username:"john", password:"mysecret"}
+    let auth = {username,password};  // {username:"john", password:"mysecret"}
 
     return User.authenticateBasic(auth)
       .then( user => _authenticate(user) );
@@ -33,6 +33,9 @@ module.exports = (req, res, next) => {
 
   function _authenticate(user) {
     if ( user ) {
+      console.log(user);
+      req.user = user;
+      // req.token = user.generateToken();
       next();
     }
     else {
